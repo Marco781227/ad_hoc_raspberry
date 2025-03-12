21c401653bac2d5536f2d004ec513c0cf1f9b793
Marco Regragui Martins <marco781227@gmail.com>
2025-03-12 19:49:49 +0100

    Finalisation des scripts pour envoyer et recevoir le nouveau cannal le moins occupé

--- envoiChangementCanal.py
+++ envoiChangementCanal.py
@@ -1,48 +1,71 @@
-import sys 
-import socket 
-import time
 import os
+import socket
+import sys
+import time
 
-MAX_ITER = 30
+MAX_ITER = 10
+CONFIRMATION = "yes"
 
-def send_channel(ip, data):
+
+def send_data(ip, port, data):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         try:
-            s.connect((ip,port))
             s.settimeout(10)
+            s.connect((ip, port))
             s.send(data.encode())
-            s.shutdown(socket.SHUT_WR) #fermeture du socket en ecriture
+            s.shutdown(socket.SHUT_WR)  # fermeture du socket en ecriture
             response = s.recv(1024)
             print(response)
             time.sleep(1)
             s.close()
             return 0
-        except OSError:
+        except Exception as e:
             print(e)
             return 1
-  
-def main():
-    port = 5001
-    receivers_ip = ['192.168.1.1','192.168.1.3']
-    bestChannel = sys.argv[1]
 
-    for ip in receivers_ip :
-        ip_not_ok = send_channel(ip, bestChannel)
+
+def send_to_servers(receivers_ip, port, data):
+    for ip in receivers_ip:
+        ip_not_ok = send_data(ip, port, data)
         nb_iter = 0
-        while(ip_not_ok and nb_iter < MAX_ITER):
-            time.sleep(5)
-            ip_not_ok = send_channel(ip, bestChannel)
+        while ip_not_ok and nb_iter < MAX_ITER:
+            time.sleep(1)
+            ip_not_ok = send_data(ip, port, data)
             nb_iter += 1
 
-        if (ip_not_ok):
+        if ip_not_ok:
             print(f"Après {MAX_ITER} essais, la connection n'a pas pu être établie)")
-            exit(1)
+            return 1
         else:
-            print(f"Connection to {ip} ok") 
+            print(f"Connection to {ip} ok")
+    return 0
 
-    os.system(f"/home/marco/Documents/tp_rsf/ad_hoc_startup.sh {bestChannel}")
+
+def main():
+    port = 5001
+    receivers_ip = ["192.168.1.1", "192.168.1.3"]
+    best_channel = sys.argv[1]
+
+    channel_not_sent = send_to_servers(receivers_ip, port, best_channel)
+
+    if channel_not_sent:
+        print(
+            "La connection n'a pas pu être établie avec toutes les machines, le cannal reste le même"
+        )
+        exit(1)
+
+    confirmation_not_sent = send_to_servers(receivers_ip, port, CONFIRMATION)
+
+    if confirmation_not_sent:
+        print(
+            "La confirmation n'a pas pu être envoyée à toutes les machines, le cannal reste le même"
+        )
+        exit(2)
+
+    os.system(f"/home/marco/Documents/tp_rsf/ad_hoc_startup.sh {best_channel}")
     print("Changement de cannal effectué")
+    time.sleep(1)
 
 
-if __name__ == '__main__' :
+if __name__ == "__main__":
     main()
---
+++ receptionChangementCanal.py
@@ -0,0 +1,40 @@
+import os
+import socket
+import time
+
+
+def receive_data(server_socket):
+    try:
+        (client_socket, _) = server_socket.accept()
+        data = client_socket.recv(1024)
+        client_socket.send(b"Bien recu")
+        print("Nouveau channel bien reçu")
+        client_socket.close()
+        return 0, data.decode()
+    except Exception as e:
+        print(e)
+        return 1, ""
+
+
+def main():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.settimeout(30)
+    server_socket.bind(("", 5001))
+    server_socket.listen(1)
+
+    reception_not_ok, best_channel = receive_data(server_socket)
+
+    if reception_not_ok:
+        print("La connection a échoué, le cannal reste le même")
+        exit(1)
+    else:
+        reception_not_ok, change_data = receive_data(server_socket)
+        if change_data:
+            print(f"Changement du cannal actuel pour {best_channel}")
+            os.system(f"/home/marco/Documents/tp_rsf/ad_hoc_startup.sh {best_channel}")
+            time.sleep(1)
+        else:
+            print("Une ou plusieurs machines n'ont pas pu être joignable, pas de changement de cannal")
+
+    server_socket.close()
