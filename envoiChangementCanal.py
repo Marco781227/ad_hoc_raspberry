02ab0c3c85bdfc2b37626ab8b4f183814edb988d
Marco Regragui Martins <marco781227@gmail.com>
2025-03-12 14:38:20 +0100

    Envoi du meilleur canal ok, en attente de mise en place du changement
    Updated last commit

--- envoiChangementCanal.py
+++ envoiChangementCanal.py
@@ -1,18 +1,42 @@
 import sys 
 import socket 
+import time
 
-port = 5001
-receivers_ip = ['192.168.1.1','192.168.1.3']
-bestChannel = sys.argv[1]
+MAX_ITER = 30
 
-def send_channel()
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #on établit la connection en IPv4 (INET) et via le protocole TCP (STREAM)
-    client.connect((ip,port)) #on se connecte au socket avec l'ip et le port
-    with open(filename, "rb") as f : #on ouvre le fichier à envoyer en lecture et en binaire pour directement l'envoyer sur le socket
-        data = f.read() 
-        if data:
-            client.send(data) #envoi du contenu du script
-    client.shutdown(socket.SHUT_WR) #fermeture du socket en ecriture
-    response = client.recv(1024) #reception du message
-    print(response.decode())
-    client.close()
+def send_channel(ip, data):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.connect((ip,port))
+            s.settimeout(10)
+            s.send(data.encode())
+            s.shutdown(socket.SHUT_WR) #fermeture du socket en ecriture
+            response = s.recv(1024)
+            print(response)
+            time.sleep(1)
+            s.close()
+            return 0
+        except OSError:
+            print(e)
+            return 1
+  
+def main():
+    port = 5001
+    receivers_ip = ['192.168.1.1','192.168.1.3']
+    bestChannel = sys.argv[1]
+
+    for ip in receivers_ip :
+        ip_not_ok = send_channel(ip, bestChannel)
+        nb_iter = 0
+        while(ip_not_ok and nb_iter < MAX_ITER):
+            time.sleep(5)
+            ip_not_ok = send_channel(ip, bestChannel)
+            nb_iter += 1
+
+        if (ip_not_ok):
+            print(f"Après {MAX_ITER} essais, la connection n'a pas pu être établie)")
+        else:
+            print("Connection ok")
+
+if __name__ == '__main__' :
+    main()
