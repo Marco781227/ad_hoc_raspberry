9951f89437e9d52acfd5d21b8e441debf22c24b5
Marco Regragui Martins <marco781227@gmail.com>
2025-03-13 18:10:37 +0100

    Modification du fichier reçoit

--- ad-hoc-startup.sh
+++ ad-hoc-startup.sh
@@ -5,5 +5,6 @@ sudo iwconfig wlan0 mode ad-hoc
 sudo iwconfig wlan0 essid "ama"
 sudo iwconfig wlan0 channel $1
 sudo ifconfig wlan0 $2 netmask 255.255.255.0 up
+sudo iwconfig
 echo "Connection ad-hoc au channel $1 à l'adresse ip $2 effectuée"
 exit 0
--- reçois.py
+++ reçois.py
@@ -3,19 +3,15 @@ import socket
 import json
 
 with socket.socket(socket.AF_INET , socket.SOCK_STREAM ) as serversocket :
-	serversocket.setsockopt(socket.SOL_SOCKET , socket.SO_REUSEADDR , 1 )
-	serversocket.bind(('',5000))  
-	serversocket.listen(1)
-	
-	(clientsocket,adress) = serversocket.accept()
-	
-	with open('blabla.py',"wb") as f : 
-		while True :
-			data = clientsocket.recv(1024)
-			if not data :
-				break
-			f.write(data)
-
+    serversocket.setsockopt(socket.SOL_SOCKET , socket.SO_REUSEADDR , 1 )
+    serversocket.bind(('',5001))  
+    serversocket.listen(1)
+    (clientsocket,adress) = serversocket.accept()
+    while True:
+        data = clientsocket.recv(1024)
+        if not data :
+            break
+        print(json.loads(data.decode()))    
 clientsocket.send(b'hohohohoo')
 print("Fichier reçus ")
 clientsocket.close()
