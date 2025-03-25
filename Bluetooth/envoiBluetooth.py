e0947dd4f5fdf302fae213b8c50481f01996857e
Marco Regragui Martins <marco781227@gmail.com>
2025-03-25 19:56:43 +0100

    Script bluetooth amélioré pour indiquer qui envoie et qui reçoit les données GPS

--- Bluetooth/envoiBluetooth.py
+++ Bluetooth/envoiBluetooth.py
@@ -1,19 +1,33 @@
 import socket
+import sys
 
-send_adress = "B8:27:EB:85:58:B8"   
+address_gps_receiver = "B8:27:EB:85:58:B8"
+address_gps_sender = "B8:27:EB:C9:02:3B"
 
-client = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM) #on établit la communicatio avec le protocole bluetooth (AF_BLUETOOTH) via le protocole TCP (SOCK_STREAM) et le protocole bluetooth RFCOMM (BTPROTO_RFCOMM)
-client.connect((send_adress, 4))
+client_gps_sender = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM) #on établit la communicatio avec le protocole bluetooth (AF_BLUETOOTH) via le protocole TCP (SOCK_STREAM) et le protocole bluetooth RFCOMM (BTPROTO_RFCOMM)
+client_gps_receiver = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM) #on établit la communicatio avec le protocole bluetooth (AF_BLUETOOTH) via le protocole TCP (SOCK_STREAM) et le protocole bluetooth RFCOMM (BTPROTO_RFCOMM)
 
 try:
-    message = "Lancer programme"
-    client.send(message.encode())
-    data = client.recv(1024)
-    print("Received message:"+data.decode())
-    data = client.recv(1024)
-    print("Received message:"+data.decode())
-except OSError as e:
-    pass
+    #Récuperation du port et de l'ip à envoyer
+    port = int(sys.argv[1])
+    receiver_ip = sys.argv[2]
+
+    client_gps_sender.connect((address_gps_sender, 4))
+    client_gps_receiver.connect((address_gps_receiver, 4))
+    
+    message_receiver = f"Listen for coordinates from: {port}"
+    message_sender = f"Send coordinates to: {receiver_ip} at port: {port}"
+    client_gps_receiver.send(message_receiver.encode())
+    client_gps_sender.send(message_sender.encode())
+
+    receiver_reply = client_gps_receiver.recv(1024)
+    sender_reply = client_gps_sender.recv(1024)
+    print("Received message from receiver:"+receiver_reply.decode())
+    print("Received message from sender:"+sender_reply.decode())
+except Exception as e:
+    print(e)
+    exit(1)
 
 
-client.close()
+client_gps_sender.close()
+client_gps_receiver.close()
--- Bluetooth/recoisBluetooth.py
+++ Bluetooth/recoisBluetooth.py
@@ -1,20 +1,21 @@
 import socket 
 import os 
 server = socket.socket(socket.AF_BLUETOOTH , socket.SOCK_STREAM , socket.BTPROTO_RFCOMM )
-server.bind(("B8:27:EB:85:58:B8",4))
+server.bind((sys.argv[1],4))
 server.listen(1)
 client,addr = server.accept()
 try :
 	while True : 
 		data = client.recv(1024)
 		if not data : break 
-		print(f"Message : {data.decode('utf-8')}")
-		message = "Ok lancement de la fusee"  
+		print(f"Received message : {data.decode('utf-8')}")
+		reply = "Bien recu"
 		client.send(message.encode("utf-8"))
 
 		os.system("python3 blabla.py")
 		client.send(("Fichier exécutable ").encode("utf-8")) 
-except OSError as e :
-	pass
+except Exception as e :
+	print(e)
+    exit(1)
 client.close()
 server.close()
