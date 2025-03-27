052419dedd969ff1c16205d7e9c1411e3fdabee4
Marco Regragui Martins <marco781227@gmail.com>
2025-03-27 16:43:49 +0100

    Avancement changement canal

--- ChangementCanal/envoyerCanalOpti.sh
+++ ChangementCanal/envoyerCanalOpti.sh
@@ -1,18 +1,23 @@
-if [[ $# -ne 1 ]]
+if [[ $# -ne 2 ]]
 then
 	echo "Usage : ./envoyerCanalOpti.sh <adresse ip>"
+	exit 1
 fi
 bestChannel=$(sudo iwlist wlan0 scan | grep Channel: | grep -Eo '([1,6,11])'| sort | uniq -c | sort -n | head -1 | awk '{print $2}')
-ip=$1
+path="$1"
+ip=$2
+chemin_complet="$path/ChangementCanal/envoiChangementCanal.py"
 commEnCours="sudo lsof -i -P -n | grep -q $ip"
 nbRepetitions=0
 LOG_FILE="$HOME/changementCanal.log"
 
+echo $chemin_complet
 while [ $nbRepetitions -ne 15 ] 
 do
 	if !(eval "$commEnCours")
 	then
-		python3 ~/Documents/tp_rsf/ChangementCanal/envoiChangementCanal.py $bestChannel $ip >> "$LOG_FILE" 2>&1
+		echo "hello"
+		python3 "$chemin_complet" "$bestChannel" "$ip" >> "$LOG_FILE" 2>&1
 		exit 0
 	fi
 	sleep 1
